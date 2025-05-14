ff1aaba3d9d276bef507cc99cbe78b432785c13b
Phan Văn <158036073+Iunhims@users.noreply.github.com>
2025-05-14 15:42:47 +0700

    Add files via upload

---
+++ Lab01.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Lab01 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // double point = sc.nextDouble();
+        // sc.nextLine();
+
+        // System.out.println("" + name + "co diem = " + point);
+// --------------------------------------------------------------------------------------
+     
+        // System.out.println("Nhap chieu dai: ");
+        // double dai = sc.nextDouble();
+        // System.out.println("Nhap chieu rong: ");
+        //  double rong = sc.nextDouble();
+        // double chuvi = (dai + rong) * 2;
+        // double dientich = dai * rong;
+        // double canhnhonhat = Math.min(dai, rong);
+
+        // System.out.println("Chu vi = " + chuvi);
+        // System.out.println("Dien tich = " + dientich);
+        // System.out.println("Canh nho nhat = " + canhnhonhat);
+// // --------------------------------------------------------------------------------------
+//         System.out.println("Nhap canh :");
+//         double canh = sc.nextDouble();
+//         System.out.println("The tich hinh lap phuong = "+ Math.pow(canh,3));
+    }
+    
+}
